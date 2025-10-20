d7d4d4f69b2efc34f41a3428427e8d6b2fac70bf
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-10-20 21:00:57 +0200

    incluyendo un menú hamburguesa para mobile

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',function(){
+    eventListeners();
+});
+
+function eventListeners(){
+    const mobilMenu=document.querySelector('.mobile-menu');
+    mobilMenu.addEventListener('click',navegacionResponsive);
+}
+
+function navegacionResponsive(){
+    const navegacion=document.querySelector('.navegacion');
+    navegacion.classList.toggle('mostrar');
+}
