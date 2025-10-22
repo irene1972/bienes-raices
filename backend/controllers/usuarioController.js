6726152401a484bdfcb8de236c08b3b544d22ec5
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-10-22 23:56:32 +0200

    añdir la funcionalidad de registrar usuario

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,9 +1,32 @@
-const registrar=(req,res)=>{
-    res.send('Desde api/usuarios');
+import pool from '../config/db.js';
+
+const registrar=async(req,res)=>{
+    let {nombre,apellidos,password,email,telefono}=req.body;
+
+    if(!email || !password){
+        res.json({msg:'Los campos EMAIL y PASSWORD son obligatorios'});
+        return;
+    }
+
+    if(!nombre) nombre='';
+    if(!apellidos) apellidos='';
+    if(!telefono) telefono='';
+
+    try {
+        const sql = `INSERT INTO usuarios (nombre, apellidos, password, email, telefono) VALUES ('${nombre}','${apellidos}','${password}','${email}','${telefono}')`;
+        const result = await pool.execute(sql, [nombre, apellidos, password,email,telefono]);
+        res.json({mensaje:`El registro se ha guardado correctamente, con id=${result[0].insertId}`});
+
+    } catch (error) {
+        console.log(error);
+        res.json({msg:'Ha habido un error'});
+    }
+
+    res.json({url:'Desde api/usuarios'});
 }
 
 const perfil=(req,res)=>{
-    res.send('Desde api/usuarios/perfil');
+    res.json({url:'Desde api/usuarios/perfil'});
 }
 
 export {
