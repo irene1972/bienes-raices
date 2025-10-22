ec29a80f0185be55b3f514a1f1f5425dbc158a22
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-10-23 01:49:34 +0200

    añadir funcionalidad confirmar usuario

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,10 +1,11 @@
 import pool from '../config/db.js';
+import generarId from '../helpers/generarId.js';
 
 const registrar=async(req,res)=>{
     let {nombre,apellidos,password,email,telefono}=req.body;
 
     if(!email || !password){
-        res.json({msg:'Los campos EMAIL y PASSWORD son obligatorios'});
+        res.status(400).json({msg:'Los campos EMAIL y PASSWORD son obligatorios'});
         return;
     }
 
@@ -12,24 +13,56 @@ const registrar=async(req,res)=>{
     if(!apellidos) apellidos='';
     if(!telefono) telefono='';
 
+    //validar que el email del usuario no esté duplicado
+    const [rows]=await pool.query(`SELECT * FROM usuarios WHERE email='${email}'`);
+    //console.log(rows);
+    
+    if(rows.length){
+        res.status(400).json({msg:'El email ya está registrado'});
+        return;
+    }
+
+    //asignar token para guardarlo en la bd
+    const token=generarId();
+
     try {
-        const sql = `INSERT INTO usuarios (nombre, apellidos, password, email, telefono) VALUES ('${nombre}','${apellidos}','${password}','${email}','${telefono}')`;
-        const result = await pool.execute(sql, [nombre, apellidos, password,email,telefono]);
+        const sql = `INSERT INTO usuarios (nombre, apellidos, password, email, telefono,token) VALUES ('${nombre}','${apellidos}','${password}','${email}','${telefono}','${token}')`;
+        const result = await pool.execute(sql, [nombre, apellidos, password,email,telefono,token]);
         res.json({mensaje:`El registro se ha guardado correctamente, con id=${result[0].insertId}`});
 
     } catch (error) {
         console.log(error);
-        res.json({msg:'Ha habido un error'});
+        res.status(400).json({msg:'Ha habido un error'});
     }
 
-    res.json({url:'Desde api/usuarios'});
 }
 
 const perfil=(req,res)=>{
     res.json({url:'Desde api/usuarios/perfil'});
 }
 
+const confirmar=async(req,res)=>{
+    const token=req.params.token;
+    const [rows]=await pool.query(`SELECT * FROM usuarios WHERE token='${token}'`);
+    if(!rows.length){
+        res.status(400).json({msg:'Token no válido'});
+        return;
+    }
+    //console.log('usuario confirmado');
+    const {id}=rows[0];
+    console.log(id);
+    const sql = `UPDATE usuarios SET token=NULL, confirmado=1 WHERE id=${id}`;
+    try {
+        await pool.execute(sql);
+        res.json({mensaje:`Se ha confirmado el usuario`});
+    } catch (error) {
+        res.status(400).json({msg:'Ha habido un error'});
+    }
+  
+}
+
 export {
     registrar,
-    perfil
+    perfil,
+    confirmar
 }
--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { registrar,perfil } from '../controllers/usuarioController.js';
+import { registrar,perfil,confirmar } from '../controllers/usuarioController.js';
 
 const router=express.Router();
 
 router.post('/',registrar);
 router.get('/perfil',perfil);
+router.get('/confirmar/:token',confirmar);
 
 export default router;
