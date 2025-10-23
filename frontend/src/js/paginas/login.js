67f9c69358abd108fe820fa6bc1599fcc0c48260
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-10-23 20:51:05 +0200

    añadir funcionalidad login

---
+++ frontend/src/js/paginas/login.js
@@ -0,0 +1,50 @@
+import { imprimirAlerta } from "../funciones.js";
+import { URL_API } from "../variables.js";
+
+(()=>{
+
+document.addEventListener('DOMContentLoaded',eventListeners);
+
+const formulario=document.querySelector('.formulario');
+
+const emailInput=document.querySelector('#email');
+const passwordInput=document.querySelector('#password');
+
+function eventListeners(){
+    formulario.addEventListener('submit',validarFormulario);
+}
+
+function validarFormulario(e){
+    e.preventDefault();
+    
+    const email=emailInput.value.trim();
+    const password=passwordInput.value.trim();
+
+    if(!email || !password){
+        imprimirAlerta('Los dos campos son obligatorios','error',formulario);
+        return;
+    }
+    iniciarSesion(email,password);
+}
+
+function iniciarSesion(email,password){
+    const usuario={email,password};
+    fetch(`${URL_API}/usuarios/login`,{
+        method:'POST',
+        body:JSON.stringify(usuario),
+        headers:{
+            'Content-Type':'application/json'
+        }
+    })
+        .then(response=>response.json())
+        .then(data=>{
+            if(data.msg){
+                imprimirAlerta(data.msg,'error',formulario);
+            }
+            //console.log(data);
+            window.location.replace(`/frontend`);
+        })
+        .catch(error=>console.error({'Error':error.message}));
+}
+
+})()
